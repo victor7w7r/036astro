bc72cc0f8b9aa08ab4036bafe8b9355228d06bac
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2023-11-30 09:56:49 -0500

    UpdatePackages
    p

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import compress from 'astro-compress';
-import prefetch from '@astrojs/prefetch';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
+import qwikdev from '@qwikdev/astro';
 
 export default defineConfig({
-  integrations: [compress(), prefetch(), svelte(), tailwind()]
+  prefetch: true,
+  integrations: [compress(), svelte(), tailwind(), qwikdev()]
 });
